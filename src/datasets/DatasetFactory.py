7d310aac252e423dc455326ff993009e8d64585c
Md. Ashraful Islam <mdashrafulpramanic@gmail.com>
2024-10-04 22:44:41 -0700

    dataset factory bug fixed

--- src/datasets/DatasetFactory.py
+++ src/datasets/DatasetFactory.py
@@ -9,17 +9,20 @@ from datasets.CodeContestDataset import CodeContestDataset
 class DatasetFactory:
     @staticmethod
     def get_dataset_class(dataset_name):
-        if dataset_name == "APPS":
+        dataset_name = dataset_name.lower()
+        if dataset_name == "apps":
             return APPSDataset
-        elif dataset_name == "MBPP":
+        elif dataset_name == "mbpp":
             return MBPPDataset
-        elif dataset_name == "XCode":
+        elif dataset_name == "xcode":
             return XCodeDataset
-        elif dataset_name == "HumanEval":
+        elif dataset_name == "xcodeeval":
+            return XCodeDataset
+        elif dataset_name == "humaneval":
             return HumanDataset
-        elif dataset_name == "Human":
+        elif dataset_name == "human":
             return HumanDataset
-        elif dataset_name == "CC":
+        elif dataset_name == "cc":
             return CodeContestDataset
         else:
             raise Exception(f"Unknown dataset name {dataset_name}")
